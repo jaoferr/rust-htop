2cf7345b4585685b8e2095ebee9ca87c942da90a
Joao Fernando <60816627+jaoferr@users.noreply.github.com>
2023-04-12 23:43:14 -0300

    Added CPU usage to root path Added futures crate for async Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use axum::{Server, Router, routing::get};
+use std::sync::Arc;
+
+use axum::{Server, Router, routing::get, extract::State};
+use futures::lock::Mutex;
+use sysinfo::{System, SystemExt, CpuExt};
 
 #[tokio::main]
 async fn main() {
     let router = Router::new()
-    .route("/", get(root_get));
+    .route("/", get(root_get))
+    .with_state(AppState {
+        sys: Arc::new(Mutex::new(System::new())),
+    });
 
     let server = Server::bind(&"0.0.0.0:7032".parse().unwrap())
     .serve(router.into_make_service());
@@ -15,7 +22,25 @@ async fn main() {
 
     println!("Hello, world!");
 }
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>,
+}
 
-async fn root_get() -> &'static str {
-    "Hi from Axum!"
+async fn root_get(State(state): State<AppState>) -> String {
+    use std::fmt::Write;
+
+    let mut s = String::new();
+
+    let mut sys = state.sys.lock().await;
+    sys.refresh_cpu();
+
+    for (i, cpu) in sys.cpus().iter().enumerate() {
+        let i = i + 1;
+        let usage = cpu.cpu_usage();
+
+        writeln!(&mut s, "CPU {i} {usage}% ").unwrap();
+    }
+
+    s
 }
