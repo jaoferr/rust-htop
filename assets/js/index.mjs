cdd7dd6f6c42aab4fcb17e608758bf87dfe4b5d1
Joao Fernando <60816627+jaoferr@users.noreply.github.com>
2023-05-21 13:18:56 -0300

    Added pure.css grid via CDN Renamed variables in index.mjs Added placeholder section for processes list

--- assets/js/index.mjs
+++ assets/js/index.mjs
@@ -5,24 +5,30 @@ const html = htm.bind(h)
 
 function App(props) {
     return html`
-    <div>
-        ${props.cpus.map((cpu, idx) => {
-            return html`
-                <div class="cpu-usage-bar">
-                    <div class="cpu-usage-bar-inner" style="width: ${cpu}%;"></div>
-                    <span class="cpu-usage-text">CPU ${idx}: ${cpu.toFixed(2)}%</span>
-                </div>`
-        })}
+    <div class="pure-g">
+        <div class="pure-u-1">
+            <p class="section-title">cpu-usage</p>
+            ${props.cpus.map((cpu, idx) => {
+                return html`
+                    <div class="cpu-usage-bar pure-u-1-4">
+                        <div class="cpu-usage-bar-inner" style="width: ${cpu}%;"></div>
+                        <span class="cpu-usage-text">CPU ${idx}: ${cpu.toFixed(2)}%</span>
+                    </div>
+                `
+            })}
+        </div>
+        <div class="pure-u-1">
+            <p class="section-title">processes</p>
+        </div>
     </div>
     `
 }
 
-let url = new URL('/ws/cpu', window.location.href)
-url.protocol = url.protocol.replace('http', 'ws')
+let urlCpuUsage = new URL('/ws/cpu', window.location.href)
+urlCpuUsage.protocol = urlCpuUsage.protocol.replace('http', 'ws')
 
-let ws = new WebSocket(url.href)
+let ws = new WebSocket(urlCpuUsage.href)
 ws.onmessage = (ev) => {
     let json = JSON.parse(ev.data)
     render(html`<${App} cpus=${json} />`, document.querySelector('preact-view'))
-
 }
