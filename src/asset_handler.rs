c79e846757191667eb25aed3907a0c5df7b860f4
Joao Fernando <60816627+jaoferr@users.noreply.github.com>
2023-05-21 19:38:31 -0300

    Added backend logic for serving assets from node_modules

--- src/asset_handler.rs
+++ src/asset_handler.rs
@@ -26,3 +26,38 @@ pub async fn get_asset(Path(path): Path<String>) -> impl IntoResponse {
             .unwrap().into_response()
     }
 }
+
+pub async fn get_npm_asset(
+    Path((asset_type, module, path)): Path<(String, String, String)>,
+) -> impl IntoResponse {
+    let asset_path: String;
+    let content_type: String;
+
+    match asset_type.as_str() {
+        "mjs" => {
+            asset_path = format!("node_modules/{}/dist/{}.module.js", module, path);
+            content_type = mime::APPLICATION_JAVASCRIPT_UTF_8.to_string();
+        },
+        "css" => {
+            asset_path = format!("node_modules/{}/build/{}", module, path);
+            content_type = mime::TEXT_CSS_UTF_8.to_string();
+        },
+        _ => {
+            asset_path = format!("node_modules/{}/dist/{}.js", module, path);
+            content_type = mime::APPLICATION_JAVASCRIPT_UTF_8.to_string();
+        }
+    }
+
+    let asset = tokio::fs::read_to_string(&asset_path).await;
+
+    if asset.is_ok() {
+        Response::builder()
+            .header("content-type", content_type)
+            .body(asset.unwrap())
+            .unwrap().into_response()
+    } else {
+        Response::builder()
+            .body(Body::empty())
+            .unwrap().into_response()
+    }
+}
