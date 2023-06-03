015009cc6b08bff039f9bf6e7c40646817fe1b32
Joao Fernando <60816627+jaoferr@users.noreply.github.com>
2023-06-03 00:15:31 -0300

    Added limit query parameter for processes route Added workaround for empty query parameters

--- src/processes.rs
+++ src/processes.rs
@@ -1,25 +1,36 @@
 use super::AppState;
 use axum::{
     extract::State,
+    extract::Query,
     response::IntoResponse,
     Json
 };
 use sysinfo::{SystemExt, ProcessExt};
+use serde::{Serialize, Deserialize};
+use super::utils::empty_string_as_none;
 
 
-#[derive(serde::Serialize)]
+#[derive(Serialize)]
 struct ProcessJSON {
     pid: String,
     process_name: String
 }
 
+#[derive(Deserialize)]
+pub struct QueryLimit {
+    #[serde(default, deserialize_with = "empty_string_as_none")]
+    limit: Option<usize>
+}
+
 #[axum::debug_handler]
-pub async fn get_processes_list(State(system_state): State<AppState>) -> impl IntoResponse {
+pub async fn get_processes_list(State(system_state): State<AppState>, query: Query<QueryLimit>) -> impl IntoResponse {
     let mut sys = system_state.sys.lock().unwrap();
     sys.refresh_processes();
     let _processes = sys.processes();
 
-    let v: Vec<_> = sys.processes().iter()
+    let limit = query.limit.unwrap_or(20);
+
+    let v: Vec<_> = sys.processes().iter().take(limit)
         .map(|p|
             ProcessJSON {
                 pid: p.0.to_string(),
